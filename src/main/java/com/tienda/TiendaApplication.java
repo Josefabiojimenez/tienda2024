da99e00ef3da7d7db63ca6e9dd44d6e84b0a5260
Admin <Admin@172.19.50.217>
2024-09-30 00:20:59 -0600

    Semana 2

--- src/main/java/com/tienda/TiendaApplication.java
+++ src/main/java/com/tienda/TiendaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoSemanalApplication {
+public class TiendaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoSemanalApplication.class, args);
+		SpringApplication.run(TiendaApplication.class, args);
 	}
 
 }
