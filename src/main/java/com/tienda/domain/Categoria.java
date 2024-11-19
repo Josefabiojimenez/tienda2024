e89304b4e54b447803bf9b4d8c13f0b83a09cec2
Admin <Admin@172.19.50.217>
2024-11-19 02:18:11 -0600

    Semana 8 finalizacion

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -3,32 +3,39 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
-@Data
+//las anotcaciones van encima de las clases, siempre
+@Data 
 @Entity
 @Table(name="categoria")
 public class Categoria implements Serializable {
-    private static final long serialVersionUID = 1L;
     
-    
+       private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
-    private Long IdCategoria;//interpretta como id_categoria
+    @Column (name="id_categoria")
+    private Long idCategoria; //interpretar  como id_categoria
     private String descripcion;
-    private String RutaImagen;
-    private boolean Activo;
+    private String rutaImagen;
+    private boolean activo;
+    
+    @OneToMany //porque una categoria puede estar en muchos productos
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)        
+    List<Producto> productos;
 
     public Categoria() {
     }
 
+    //no se hace con el id de categoria porque para eso esta la base de datos
     public Categoria(String descripcion, String rutaImagen, boolean activo) {
         this.descripcion = descripcion;
-        this.RutaImagen = rutaImagen;
-        this.Activo = activo;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
     }
-
+    
+    
     
 }
